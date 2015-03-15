91efe8d98f9a283dab8d51f333b0af72293ec565
Kevin Markham <justmarkham@gmail.com>
2015-03-15 12:22:11 -0400

    update code from intermediate workshop

--- code/00_python_intermediate_workshop.py
+++ code/00_python_intermediate_workshop.py
@@ -5,37 +5,64 @@ b = 5.0
 c = a/2
 d = b/2
 
-# What is type(a)?
-# What is type(b)?
-# What is c?
-# What is d?
+'''
+What is type(a)?
+  int
+What is type(b)?
+  float
+What is c?
+  2
+What is d?
+  2.5
+'''
 
 e = [a, b]
 f = range(10)
 
-# What is type(e)?
-# What is len(e)?
-# What is type(f)?
-# What are the contents of f?
-# What is 'range' called?
-# How do I get help on 'range'?
+'''
+What is type(e)?
+  list
+What is len(e)?
+  2
+What is type(f)?
+  list
+What are the contents of f?
+  integers 0 through 9
+What is 'range' called?
+  a function
+How do I get help on 'range'?
+  help(range)
+'''
 
 g = ['mon','tues','wed','thurs','fri']
 
-# How do I slice out 'mon'?
-# How do I slice out 'mon' through 'wed'?
-# What are two ways to slice out 'fri'?
-# How do I check the type of 'mon'?
+'''
+How do I slice out 'mon'?
+  g[0]
+How do I slice out 'mon' through 'wed'?
+  g[0:3]
+What are two ways to slice out 'fri'?
+  g[4] or g[-1]
+How do I check the type of 'mon'?
+  type(g[0])
+'''
 
 g.remove('wed')
 sorted(g)
 h = sorted(g, reverse=True)
 
-# What are the contents of g?
-# What are the contents of h?
-# What is 'remove' called?
-# How do I get help on 'remove'?
-# What is 'reverse=True' called?
+'''
+What are the contents of g?
+  ['mon','tues','thurs','fri']
+What are the contents of h?
+  ['tues','thurs','mon','fri']
+What is 'remove' called?
+  a list method
+How do I get help on 'remove'?
+  help(g.remove)
+What is 'reverse=True' called?
+  an optional argument
+'''
 
 i = 'Hello'
 j = 'friend'
@@ -43,9 +70,14 @@ k = i + j
 l = i + 3
 m = i[0]
 
-# What is 'k'?
-# What is 'l'?
-# What is 'm'?
+'''
+What is 'k'?
+  'Hellofriend'
+What is 'l'?
+  undefined (due to error)
+What is 'm'?
+  'H'
+'''
 
 
 
@@ -64,17 +96,26 @@ for num in nums:
 # equivalent list comprehension
 cubes = [num**3 for num in nums]    # [1, 8, 27, 64, 125]
 
-# EXERCISE:
-# Given that: letters = ['a','b','c']
-# Write a list comprehension that returns: ['A','B','C']
+'''
+EXERCISE:
+Given that: letters = ['a','b','c']
+Write a list comprehension that returns: ['A','B','C']
+Hint: 'hello'.upper() returns 'HELLO'
 
-# BONUS EXERCISE:
-# Given that: word = 'abc'
-# Write a list comprehension that returns: ['A','B','C']
+[letter.upper() for letter in letters]
+
+BONUS EXERCISE:
+Given that: word = 'abc'
+Write a list comprehension that returns: ['A','B','C']
+
+[letter.upper() for letter in word]
+'''
 
 
 
 ## LIST COMPREHENSIONS WITH CONDITIONS
+
+nums = range(1, 6)
 
 # for loop to create a list of cubes of even numbers
 cubes_of_even = []
@@ -97,8 +138,12 @@ cubes_of_even = [num**3 for num in nums if num % 2 == 0]    # [8, 64]
 
 # dictionaries are different from lists:
 # - dictionaries are unordered
-# - dictionaries are made of key-value pairs
-# - dictionary keys must be unique
+# - dictionary lookup time is constant regardless of dictionary size
+
+# dictionaries are like real dictionaries:
+# - dictionaries are made of key-value pairs (word and definition)
+# - dictionary keys must be unique (each word is only defined once)
+# - you can use the key to look up the value, but not the other way around
 
 # create a dictionary (and open Variable Explorer in Spyder)
 family = {'dad':'homer', 'mom':'marge', 'size':6}
@@ -111,8 +156,6 @@ family.keys()       # returns list: ['dad', 'mom', 'size']
 family.values()     # returns list: ['homer', 'marge', 6]
 family.items()      # returns list of tuples:
                     #   [('dad', 'homer'), ('mom', 'marge'), ('size', 6)]
-'mom' in family     # returns True
-'marge' in family   # returns False (only checks keys)
 
 # modify a dictionary
 family['cat'] = 'snowball'          # add a new entry
@@ -123,15 +166,24 @@ family['kids'] = ['bart', 'lisa']   # value can be a list
 # accessing a list element within a dictionary
 family['kids'][0]   # returns 'bart'
 
-# EXERCISE:
-# Given that: d = {'a':10, 'b':20, 'c':[30, 40]}
-# First, print the value for 'a'
-# Then, change the value for 'b' to be 25
-# Then, change the 30 to be 35
-# Finally, append 45 to the end of the list that contains 35 and 40
+'''
+EXERCISE:
+Given that: d = {'a':10, 'b':20, 'c':[30, 40]}
+First, print the value for 'a'
+Then, change the value for 'b' to be 25
+Then, change the 30 to be 35
+Finally, append 45 to the end of the list that contains 35 and 40
 
-# BONUS EXERCISE:
-# Write a list comprehension that returns a list of the keys in uppercase
+d['a']
+d['b'] = 25
+d['c'][0] = 35
+d['c'].append(45)
+
+BONUS EXERCISE:
+Write a list comprehension that returns a list of the keys in uppercase
+
+[key.upper() for key in d.keys()]
+'''
 
 
 
@@ -163,8 +215,8 @@ pprint.pprint(top)
 artists = top['response']['artists']    # list of 15 dictionaries
 
 # reformat data into a table structure
-artists_header = artists[0].keys()                      # list of 2 strings
 artists_data = [artist.values() for artist in artists]  # list of 15 lists
+artists_header = artists[0].keys()                      # list of 2 strings
 
 
 
@@ -175,14 +227,16 @@ artists_data = [artist.values() for artist in artists]  # list of 15 lists
 # FiveThirtyEight data: https://github.com/fivethirtyeight/data
 # NFL ticket prices data: https://github.com/fivethirtyeight/data/tree/master/nfl-ticket-prices
 
-# open a downloaded CSV file
-import csv
-with open('2014-average-ticket-price.csv', 'rU') as f:
-    data = [row for row in csv.reader(f)]
+# Question: What is the average ticket price for Ravens' home vs away games?
 
 # open a CSV file from a URL
+import csv
 r = requests.get('https://raw.githubusercontent.com/fivethirtyeight/data/master/nfl-ticket-prices/2014-average-ticket-price.csv')
-data = [row for row in csv.reader(r.iter_lines())]
+data = [row for row in csv.reader(r.iter_lines())]      # list of lists
+
+# open a downloaded CSV file from your working directory
+with open('2014-average-ticket-price.csv', 'rU') as f:
+    data = [row for row in csv.reader(f)]       # list of lists
 
 # examine the data
 type(data)
@@ -192,8 +246,6 @@ data[1]
 
 # save the data we want
 data = data[1:97]
-
-# Question: What is the average ticket price for Ravens' home vs away games?
 
 # step 1: create a list that only contains events
 data[0][0]
@@ -225,15 +277,14 @@ for event in events:
     stop = event.find(' Tickets ')
     home_teams.append(event[start:stop])
 
-# step 6: create a list of the Ravens home game prices
-zip(home_teams, prices)
-[pair for pair in zip(home_teams, prices)]
-[pair[1] for pair in zip(home_teams, prices)]
-[price for team, price in zip(home_teams, prices)]
-[price for team, price in zip(home_teams, prices) if team == 'Baltimore Ravens']
-ravens_home = [price for team, price in zip(home_teams, prices) if team == 'Baltimore Ravens']
+# step 6: figure out how to get prices only for Ravens home games
+zip(home_teams, prices)     # list of tuples
+[pair[1] for pair in zip(home_teams, prices)]   # iterate through tuples and get price
+[price for team, price in zip(home_teams, prices)]  # better way to get price
+[price for team, price in zip(home_teams, prices) if team == 'Baltimore Ravens']    # add a condition
 
-# step 7: create a list of the Ravens away game prices
+# step 7: create lists of the Ravens home and away game prices
+ravens_home = [price for team, price in zip(home_teams, prices) if team == 'Baltimore Ravens']
 ravens_away = [price for team, price in zip(away_teams, prices) if team == 'Baltimore Ravens']
 
 # EXERCISE
