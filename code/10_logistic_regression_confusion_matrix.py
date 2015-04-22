4e7a5d0b3f66d895a6895815f09b59cb947af35a
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2015-04-22 19:11:39 -0400

    Updated exercises

--- code/10_logistic_regression_confusion_matrix.py
+++ code/10_logistic_regression_confusion_matrix.py
@@ -52,7 +52,7 @@ print metrics.accuracy_score(y_test, y_pred)
 
 # 1. Create a vector of majority class and use the accuracy_score.
 # "If I predicted all 0's, how accurate would I be?
-print metrics.accuracy_score(y_test, [0]*len(y_pred))
+print metrics.accuracy_score(y_test, [0]*len(y_test))
 
 # 2. Calculate the mean of y_test (AKA the percentage of 1's)
 y_test.mean()
@@ -110,7 +110,7 @@ print specificity
 # strong.
 
 # Predict probabilities
-logreg.predict_proba(X_test)
+logreg.predict_proba(X_test).shape
 probs = logreg.predict_proba(X_test)[:, 1]
 
 # The natural threshold for probabilility is 0.5, but you don't have to use 
@@ -119,7 +119,7 @@ probs = logreg.predict_proba(X_test)[:, 1]
 # Use 0.5 thrshold for predicting 'default' and confirm we get the same results
 preds_05 = np.where(probs >= 0.5, 1, 0)
 print metrics.accuracy_score(y_test, preds_05)
-con_mat_05 = metrics.confusion_matrix(list(y_test), list(preds_05))
+con_mat_05 = metrics.confusion_matrix(y_test, preds_05)
 print con_mat_05
 
 # Let's look at a histogram of these probabilities.
@@ -132,11 +132,11 @@ plt.show()
 # Change cutoff for predicting default to 0.2
 preds_02 = np.where(probs > 0.2, 1, 0)
 delta = float((preds_02 != preds_05).sum())/len(X_test)*100
-print 'Changing the threshold from 0.5 to 0.2 change %.2f percent of the predictions.' % delta
+print 'Changing the threshold from 0.5 to 0.2 changed %.2f percent of the predictions.' % delta
 
 # Check the new accuracy, sensitivity, specificity
 print metrics.accuracy_score(y_test, preds_02)
-con_mat_02 = metrics.confusion_matrix(list(y_test), list(preds_02))
+con_mat_02 = metrics.confusion_matrix(y_test, preds_02)
 print con_mat_02
 
 # Let's define our true posititves, false positives, true negatives, and false negatives
@@ -164,17 +164,88 @@ Let's use the glass identification dataset again.   We've previously run knn
 on this dataset.  Now, let's try logistic regression.  Access the dataset at
 http://archive.ics.uci.edu/ml/datasets/Glass+Identification.  Complete the 
 following tasks or answer the following questions.
-
+'''
+'''
 1. Read the data into a pandas dataframe.
+'''
+# Taken from Kevin's 07 HW solution
+df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/glass/glass.data',
+                 names=['id','ri','na','mg','al','si','k','ca','ba','fe','glass_type'],
+                 index_col='id')
+
+''''
 2. Explore the data and look at what columns are available.
+'''
+# Taken from Kevin's 07 HW solution
+df.shape # 214 x 10
+df.head()
+df.tail()
+df.glass_type.value_counts()
+df.isnull().sum() # No nulls in our data
+
+''''
 3. Convert the 'glass type' column into a binary response.
     * If type of class = 1/2/3/4, binary=0.
     * If type of glass = 5/6/7, binary=1.
-4. Create a feature matrix and a response vector.  
+'''
+# Taken from Kevin's 07 HW solution
+df['binary'] = np.where(df.glass_type < 5, 0, 1)                        # method 1
+df['binary'] = df.glass_type.map({1:0, 2:0, 3:0, 4:0, 5:1, 6:1, 7:1})   # method 2
+df.binary.value_counts()
+
+'''
+4. Create a feature matrix and a response vector. 
+'''
+# Taken from Kevin's 07 HW solution
+features = ['ri','na','mg','al','si','k','ca','ba','fe']    # create a list of features
+features = df.columns[:-2]      # alternative way: slice 'columns' attribute like a list
+X = df[features]                # create DataFrame X by only selecting features
+y = df.binary
+
+'''
 5. Split the data into the appropriate training and testing sets.
+'''
+# Taken from Kevin's 07 HW solution
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=99)
+
+'''
 6. Create and fit a logistic regression model.
+'''
+logreg = LogisticRegression() # Instatiate estimator
+logreg.fit(X_train, y_train) # Fit data
+
+'''
 7. Make predictions with your new model.
+'''
+y_pred = logreg.predict(X_test) # Create predictions
+
+'''
 8. Calculate the accuracy rate of your model and compare it to the null accuracy.
+'''
+# Calculate accuracy of model
+metrics.accuracy_score(y_test, y_pred)  
+
+# Calculate null accuracy
+metrics.accuracy_score(y_test, [0]*len(y_test))
+
+'''
 9. Generate a confusion matrix for your predictions.  Use this to calculate the
 sensitivity and specificity of your model.
 '''
+# Let's look at the confusion matrix
+con_mat = metrics.confusion_matrix(y_test, y_pred)
+print con_mat
+
+# Let's define our true posititves, false positives, true negatives, and false negatives
+true_neg = con_mat[0][0]
+false_neg = con_mat[1][0]
+true_pos = con_mat[1][1]
+false_pos = con_mat[0][1]
+
+# Sensitivity: percent of correct predictions when reference value is 'default'
+sensitivity = float(true_pos)/(false_neg + true_pos)
+print sensitivity
+
+# Specificity: percent of correct predictions when reference value is 'not default'
+specificity = float(true_neg) / (true_neg + false_pos)
+print specificity
