322b0222aa6149cdb857f04dac8c107ee36e5665
Brandon M. Burroughs <brandonmburroughs@gmail.com>
2015-05-17 22:27:45 -0400

    Updated clustering code

--- code/18_clustering.py
+++ code/18_clustering.py
@@ -1,9 +1,8 @@
 '''
-CLUSTER ANALYSIS
-How do we implement a k-means clustering algorithm?
+THE DATA
 
-scikit-learn KMeans documentation for reference:
-http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+We have data about cars:  things like MPG, acceleration, weight, etc.  However,
+we don't have labels for these groups.  We want to be able to label
 '''
 
 # Imports
@@ -12,23 +11,40 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+# Read in data
+data = pd.read_table('auto_mpg.txt', sep='|') # All values range from 0 to 1
+data.drop('car_name', axis=1, inplace=True) # Drop labels from dataframe
+data.head()
+
+
+
+'''
+CLUSTER ANALYSIS
+How do we implement a k-means clustering algorithm?
+
+scikit-learn KMeans documentation for reference:
+http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+'''
+
+# Standardize our data
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+data_scaled = scaler.fit_transform(data)
+
+
 # Set random seed for reproducibility 
 np.random.seed(0)
 
-# Read in data
-data = pd.read_csv('student_data.csv') # All values range from 0 to 1
-data.head() # Look at data
-data.describe() # Get summary of data
-
-# Clean up the data
-manual_labels = data.manual_label # Store labels
-data.drop('manual_label', axis=1, inplace=True) # Drop labels from dataframe
-data.head() # Confirm it was dropped
-
 # Run KMeans
-est = KMeans(n_clusters=4, init='random') # Instatiate estimator
-est.fit(data) # Fit your data
-y_kmeans = est.predict(data) # Make cluster "predictions"
+est = KMeans(n_clusters=2, init='random') # Instatiate estimator
+est.fit(data_scaled) # Fit your data
+y_kmeans = est.predict(data_scaled) # Make cluster "predictions"
+
+# Inspect the data by looking at the means for each cluster
+data.groupby(y_kmeans).mean()
+
+# This can be compared to the overall means for each variable
+data.mean()
 
 # We can get the coordiantes for the center of each cluster
 centers = est.cluster_centers_
@@ -43,15 +59,17 @@ VISUALIZING THE CLUSTERS
 colors = np.array(['red', 'green', 'blue', 'yellow', 'orange'])
 
 plt.figure()
-plt.scatter(data.iloc[:, 2], data.iloc[:, 3], c=colors[y_kmeans], s=50)
-plt.xlabel('Study Time')
-plt.ylabel('Exam Performance')
-plt.scatter(centers[:, 2], centers[:, 3], linewidths=3, marker='+', s=300, c='black')
+plt.scatter(data_scaled[:, 0], data_scaled[:, 5], c=colors[y_kmeans], s=50)
+plt.xlabel('MPG')
+plt.ylabel('Acceleration')
+plt.scatter(centers[:, 0], centers[:, 5], linewidths=3, marker='+', s=300, c='black')
 plt.show()
 
 # We can generate a scatter matrix to see all of the different dimensions paired
 pd.scatter_matrix(data, c=colors[y_kmeans], figsize=(15,15), s = 100)
 plt.show()
+
+
 
 '''
 DETERMINING THE NUMBER OF CLUSTERS
@@ -76,8 +94,7 @@ from sklearn import metrics
 silhouette_score = [metrics.silhouette_score(data, e.labels_, metric='euclidean') for e in k_est]
 
 # Plot the results
-plt.figure(figsize=(7, 8))
-plt.subplot(211)
+plt.figure()
 plt.title('Silhouette coefficient for various values of k')
 plt.plot(k_rng, silhouette_score, 'b*-')
 plt.xlim([1,15])
